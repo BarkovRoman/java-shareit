cbe5a188991970c8244c6c25121284bf4d11b190
BarkovRoman <roma.barckow@yandex.ru>
2022-12-13 20:03:29 +0300

    fix: исправление замечаний

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.item.repository;
-
-import ru.practicum.shareit.item.model.Item;
-
-public interface ItemRepositoryCustom {
-    Item update(Long userId, Long itemId, Item item);
-}
